52e81b8c5fc10203d14a648a9926f96f21505b0a
Brian Garland <bri.garland@gmail.com>
2026-01-13 14:14:58 -0800

    add @ alias for src folder for easier imports

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import type { IJobAnalysisRequest, IAnalysisState } from '../../types';
+import type { IJobAnalysisRequest, IAnalysisState } from '@/types';
 import styles from './Home.module.scss';
 
 function Home() {
